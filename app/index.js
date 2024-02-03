64661b45fc665b22f13d02ae7fd93dd7bb833174
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-03 15:50:26 -0600

    Added basic imports from other files to display

--- app/index.js
+++ app/index.js
@@ -2,35 +2,44 @@ import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
 import { useState } from "react";
 import { Stack, useRouter } from "expo-router";
 
-export default function Page() {
+import { COLORS, icons, images, SIZES } from '../constants'
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
+const Home = () => {
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen 
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="100%" />
+          ),
+          headerTitle: ""
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}>
+
+            
+            <Welcome />
+            <Popularjobs />
+            <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+
+export default Home
